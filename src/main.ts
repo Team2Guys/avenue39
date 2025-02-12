7df3a8971d153653fdc9498bf9a85e8ddcc8077c
MFaad1 <faadsardar123@gmail.com>
2025-02-12 10:50:52 +0000

    fixes

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://avenue39-eight.vercel.app',
+      "https://avenue39.com/",
       'https://avenue39-eight.vercel.app/',
       'http://192.168.1.18:3000',
       'http://185.151.51.28:5004'
@@ -26,7 +26,7 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('backend/api');
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());

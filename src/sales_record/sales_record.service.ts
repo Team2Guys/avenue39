835c7d45293d4933b866e23f5d039091a7fe53bc
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-02-14 14:58:28 +0500

    fixes:mail

--- src/sales_record/sales_record.service.ts
+++ src/sales_record/sales_record.service.ts
@@ -718,7 +718,6 @@ export class SalesRecordService {
 
     try {
 
-      // const recipients = `mujtaba.shafique01@gmail.com`
       const recipients = email
         ? `${email}`
         : `${process.env.RECEIVER_MAIL1}, ${process.env.RECEIVER_MAIL2}`;

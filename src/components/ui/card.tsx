ecbfd8e24839974f3881a09a916b31d85cadc27a
Wajid-Fareed <wajidfareed103@gmail.com>
2025-04-24 15:38:37 +0500

    fixes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,11 +1,10 @@
 'use client';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch, State } from '@redux/store';
-import { CartItem } from '@cartSlice/types';
+import { CartItem, CartSize } from '@cartSlice/types';
 import {
-  calculateRatingsPercentage,
   generateSlug,
   getProductStock,
   variationName,
@@ -45,42 +44,66 @@ const Card: React.FC<CardProps> = ({
 }) => {
   const dispatch = useDispatch<Dispatch>();
   const cartItems = useSelector((state: State | any) => state.cart.items);
-
+  const [itemToAdd, setItemToAdd] = useState<CartItem | null>(null);
   // Memoized values to prevent unnecessary calculations
-  const displayTag = useMemo(() => cardProductTags.find((item) => item.name === card?.name), [card?.name]);
+  const [displayInfo, setDisplayInfo] = useState<{ displayName: string | undefined, displayTag: { name: string; tagPara: string; displayName: string; } | undefined, uniqueSizes: Sizes[] }>({ displayName: '', displayTag: undefined, uniqueSizes: [] });
+  const [totalStock, setTotalStock] = useState<number>(0);
+  const [isOutStock, setIsOutStock] = useState<boolean>(false);
+  useEffect(() => {
+    if (!card) return;
+    const displayName = variationName({ product: card });
+    const displayTag = cardProductTags.find((item) => item.name === card?.name);
+    const uniqueSizes = [...new Map(card.sizes?.map((size: Sizes) => [size.name, size])).values()];
+    setDisplayInfo({ displayName, displayTag, uniqueSizes });
+  }, [card]);
 
-  const uniqueSizes = useMemo(() => {
-    if (!card?.sizes?.length) return [];
-    return [...new Map(card.sizes.map((size: Sizes) => [size.name, size])).values()];
-  }, [card?.sizes]);
-
-  const displayName = useMemo(() => (card ? variationName({ product: card }) : ''), [card]);
-
-  const itemToAdd: CartItem | any = useMemo(() => ({
-    ...card,
-    quantity: 1,
-    selectedSize: card?.sizes?.find((size) => size.name === card.sizeName)
-      ?? ((isHomepage || slider) ? card?.sizes?.[0] : undefined),
-    selectedfilter: card?.filter?.[0]?.additionalInformation?.find((f) => f.name === card.colorName)
-      ?? ((isHomepage || slider) ? card?.filter?.[0]?.additionalInformation?.[0] : undefined),
-    selectedShipping: card?.shippingOptions?.[0] || null,
-  }), [card, isHomepage, slider]);
-
-  const totalStock = useMemo(() => getProductStock({ product: itemToAdd }), [itemToAdd]);
-  const isOutStock = totalStock <= 0;
-
-  const [averageRating, setAverageRating] = useState<number>();
-  const cardStaticData = useMemo(
-    () => productImages?.find((item) => item.name === card?.name),
-    [productImages, card?.name]
-  );
 
   useEffect(() => {
-    if (card?.reviews?.length) {
-      const { averageRating } = calculateRatingsPercentage(card.reviews);
-      setAverageRating(averageRating);
-    }
-  }, [card?.reviews]);
+    if (!card) return;
+
+    const sizes = card?.sizes?.find(
+      (size) => size.name === card.sizeName
+    ) ?? ((isHomepage || slider) ? card?.sizes?.[0] : undefined);
+    const selectedSize = sizes as CartSize | undefined;
+    const selectedfilter = card?.filter?.[0]?.additionalInformation?.find(
+      (f) => f.name === card.colorName
+    ) ?? ((isHomepage || slider) ? card?.filter?.[0]?.additionalInformation?.[0] : undefined);
+
+    const selectedShipping = card?.shippingOptions?.[0] || undefined;
+
+    setItemToAdd({
+      ...card,
+      quantity: 1,
+      selectedSize,
+      selectedfilter,
+      selectedShipping
+    });
+  }, [card, isHomepage, slider]);
+
+
+
+  useEffect(() => {
+    if (!itemToAdd) return;
+  
+    const total = getProductStock({ product: itemToAdd as CartItem });
+    setTotalStock(total);
+    setIsOutStock(total <= 0);
+  }, [itemToAdd]);
+  
+
+
+  const cardStaticData = useMemo(() => {
+    return productImages?.find((item) => item.name === card?.name);
+  }, [productImages, card?.name]);
+
+  
+  // const [averageRating, setAverageRating] = useState<number>();
+  // useEffect(() => {
+  //   if (card?.reviews?.length) {
+  //     const { averageRating } = calculateRatingsPercentage(card.reviews);
+  //     setAverageRating(averageRating);
+  //   }
+  // }, [card?.reviews]);
 
   const baseUrl = useMemo(() => ChangeUrlHandler(card, SubcategoryName?.name, mainCatgory), [
     card,
@@ -88,16 +111,21 @@ const Card: React.FC<CardProps> = ({
     mainCatgory,
   ]);
 
-  const finalUrl = useMemo(() => {
+  const generateFinalUrl = useCallback(() => {
     const params = new URLSearchParams();
-    if (itemToAdd.selectedfilter?.name) {
+  
+    if (itemToAdd?.selectedfilter?.name) {
       params.set('variant', generateSlug(itemToAdd.selectedfilter.name));
     }
-    if (itemToAdd.selectedSize?.name) {
+    if (itemToAdd?.selectedSize?.name) {
       params.set('size', generateSlug(itemToAdd.selectedSize.name));
     }
-    return `${baseUrl}${params.toString() ? `?${params.toString()}` : ''}`;
+  
+    const query = params.toString();
+    return query ? `${baseUrl}?${query}` : baseUrl;
   }, [itemToAdd, baseUrl]);
+  
+  
 
   const handleAddToCard = (e: React.MouseEvent<HTMLElement>) =>
     handleAddToCartCard(e, card, itemToAdd, cartItems, totalStock, dispatch);
@@ -112,18 +140,18 @@ const Card: React.FC<CardProps> = ({
   return cardLayout === 'grid' ? (
     <PortraitCard
       accessoriesSlider={accessoriesSlider}
-      averageRating={averageRating}
+      averageRating={0}
       calculateHeight={calculateHeight}
       card={card}
       cardImageHeight={cardImageHeight}
       cardStaticData={cardStaticData}
       className={className}
-      displayName={displayName}
-      displayTag={displayTag}
-      finalUrl={finalUrl}
+      displayName={displayInfo.displayName}
+      displayTag={displayInfo.displayTag}
+      finalUrl={generateFinalUrl()}
       handleAddToCard={handleAddToCard}
       handleAddToWishlist={handleAddToWishlist}
-      handleEventProbation={(e : any) => e.stopPropagation()}
+      handleEventProbation={(e: any) => e.stopPropagation()}
       isHomepage={isHomepage}
       isLandscape={isLandscape}
       isModel={isModel}
@@ -132,20 +160,20 @@ const Card: React.FC<CardProps> = ({
       portSpace={portSpace}
       skeletonHeight={skeletonHeight}
       slider={slider}
-      uniqueSizes={uniqueSizes}
+      uniqueSizes={displayInfo.uniqueSizes}
       fill={fill}
     />
   ) : (
     <LandScapeCard
       card={card}
-      finalUrl={finalUrl}
+      finalUrl={generateFinalUrl()}
       handleAddToWishlist={handleAddToWishlist}
-      averageRating={averageRating}
-      displayName={displayName}
+      averageRating={0}
+      displayName={displayInfo.displayName}
       handleAddToCard={handleAddToCard}
       handleEventProbation={(e: any) => e.stopPropagation()}
       itemToAdd={itemToAdd}
-      uniqueSizes={uniqueSizes}
+      uniqueSizes={displayInfo.uniqueSizes}
     />
   );
 };
